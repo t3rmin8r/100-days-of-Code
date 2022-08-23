f9897ef6fe2bff9fd9b352366b47b2539afc83ef
Joe Holloway <josephhllwy357@gmail.com>
2022-08-22 22:32:14 -0400

    main.py exercise 1-4

--- Day 1/main.py
+++ Day 1/main.py
@@ -2,3 +2,53 @@ print('Hello World!')
 
 #double quotes denotes strings and single quotes can do the same thing but best to use double quotes
 
+
+
+
+
+
+#Exercise 2
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the \'+\' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+
+
+#Exercise 3
+#Write your code below this line 👇
+
+
+
+
+name = input("What is your name?")
+print(len(name))
+
+
+
+
+#Exercise 4
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
