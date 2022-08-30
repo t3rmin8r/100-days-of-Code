1946d4bdd39a14aac40629c074ea3b84b652e1f6
Joseph Holloway <terminator@T3RMIN8R.local>
2022-08-29 21:26:38 -0400

    started day 3 - added even number check

---
+++ Day 3/even_or_odd.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# basic syntax search lead me to a quick understanding
+# accepting input and checking if the number has a remainder
+# if not - prints "Your number is odd!"
+
+if number % 2 == 0:
+    print("Your number is even!")
+else:
+    print("Your number is odd!")
