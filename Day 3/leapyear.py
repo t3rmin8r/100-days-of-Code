7ac0bc245116b23a64689ccad88ebb0bd8a03f14
Joseph Holloway <terminator@T3RMIN8R.local>
2022-09-01 22:00:38 -0400

    added instructor code to leap year

--- Day 3/leapyear.py
+++ Day 3/leapyear.py
@@ -14,3 +14,16 @@ if year % 4 == 0:
         print("Leap year.")
 else:
     print("Not leap year.")
+
+
+### Instructor code below
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not leap year.")
