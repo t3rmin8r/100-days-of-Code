585ca4beb260da03dd01dc520097c7f6e24c8215
Joseph Holloway <terminator@T3RMIN8R.local>
2022-09-01 22:29:55 -0400

    add updated code from Day 3 finishing tmrw

---
+++ Day 3/rollercoaster.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# Pizza Size base prices
+small_pizza = 15
+medium_pizza = 20
+large_pizza = 25
+
+# ingredient pricing
+pepperoni_medium = 2
+pepperoni_medium = 3
+pepperoni_large = 3
+extra_cheese = 1
+
+#🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨(Left off here)
+#Calculate for inputs
+if size == S
+    if
