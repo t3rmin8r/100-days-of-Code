2240dbd91cd1670f73a94f2fc273ec108afc756d
Joseph Holloway <terminator@T3RMIN8R.local>
2022-09-02 22:33:58 -0400

    added new pizza order code - not finished though

--- Day 3/pizza_order_prac.py
+++ Day 3/pizza_order_prac.py
@@ -1,4 +1,8 @@
 # 🚨 Don't change the code below 👇
+from ast import And
+from audioop import add
+from cgitb import small
+from re import S
 from tkinter import Y
 
 
@@ -10,4 +14,38 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 
 #Write your code below this line 👇
 
-if size == S:
+total_bill = 0
+
+# Pizza Size base prices
+S = 15
+M = 20
+L = 25
+
+# ingredient pricing
+pep_small = 2
+pep_medium = 3
+pep_large = 3
+extra_cheese = 1
+
+
+#Calculate for inputs
+if size == "S":
+    total_bill += S
+
+wants_pep = add_pepperoni
+if wants_pep == "Y":
+    total_bill += pep_small
+
+wants_extra_cheese = extra_cheese
+if wants_extra_cheese == "Y":
+    total_bill += 
+else:
+    total_bill = total_bill
+# if size == "M": 
+#     total_bill += M
+# if size == "L":
+#     total_bill += L
+
+
+
+print(f"Your final bill is: ${total_bill}")
