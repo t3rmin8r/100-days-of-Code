3afb1747f13c4ea5df07f40e861695b596fce2c8
Joe Holloway <josephhllwy357@gmail.com>
2022-09-10 19:21:35 -0400

    added the code to treasure_island.py

--- Day 3/treasure_island.py
+++ Day 3/treasure_island.py
@@ -1,15 +1,15 @@
 print('''
 *******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
+__________|                   |                  |                     |
+__________|________________.=""_;=.______________|_____________________|_______
 |                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|___________________|__"=._o`"-._        `"=.______________|___________________|
+__________|                `"=._o`"=._      _`"=._                     |
+__________|_____________________:=._o "=._."_.-="'"=.__________________|_______
 |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
 |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|_________|           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+__________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
 |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
 |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
 ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
@@ -17,7 +17,6 @@ ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
 ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
 /______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
 ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
 print("Welcome to Treasure Island.")
@@ -27,5 +26,19 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
-which_way = input(print("Which way do you want to go L or R?"))
-boat_or_swim = input(print("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across."))
+which_way = input('You are at a crossroad. Which way do you want to go "L" or "R"?')
+which_way = which_way.lower()
+if which_way == "r":
+    print("Game Over")
+elif which_way == "l":
+    lake_choice = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.')
+    if lake_choice == "swim":
+        print("Game Over")
+    elif lake_choice == "wait":
+        island_door_choice = input('You arrive at the island unharmed. There is a house with 3 doors. One "red", one "yellow" and one "blue". Which colour do you choose?')
+        if island_door_choice == "red" or island_door_choice == "blue":
+            print("Game Over")
+        elif island_door_choice == "yellow":
+            print("You Win!")
+        else:
+            print("You done did it now!")
