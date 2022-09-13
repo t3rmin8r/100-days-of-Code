532aef86cc953fe34bd40c6d174ed8e03003254c
Joseph Holloway <terminator@T3RMIN8R.local>
2022-09-12 21:39:27 -0400

    added the correct code back cause the other was there to confuse you

--- Day 4/heads_or_tails.py
+++ Day 4/heads_or_tails.py
@@ -1,29 +1,30 @@
 #Write your code below this line 👇
 #Hint: Remember to import the random module first. 🎲
-
-
-### Here is the basic form
-#
-# import random
-
-# heads = 1
-# tails = 0
-# toss = random.randint(0, 1)
-
-# if toss == 0:
-#     print("Tail")
-# else:
-#     print("Heads")
-#
-
+import random
 
 #Remember to use the random module
 #Hint: Remember to import the random module here at the top of the file. 🎲
 	 
 # 🚨 Don't change the code below 👇
-test_seed = int(input("Create a seed number: "))
-random.seed(test_seed)
+# test_seed = int(input("Create a seed number: "))
+# random.seed(test_seed)
  # 🚨 Don't change the code above 👆 It's only for testing your code.
 	 
 #Write the rest of your code below this line 👇
+
+
+### Here is the basic form and correct. Above is confusing not necessary
+#
+
+
+heads = 1
+tails = 0
+toss = random.randint(0, 1)
+
+
+if toss == 0:
+    print("Tail")
+else:
+    print("Heads")
+
 
