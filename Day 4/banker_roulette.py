db7c2fa09399e0f164dd0d3820ac7c2e791840a5
Joseph Holloway <terminator@T3RMIN8R.local>
2022-09-25 19:27:20 -0400

    added the banker_roulette.py

--- Day 4/banker_roulette.py
+++ Day 4/banker_roulette.py
@@ -4,16 +4,35 @@
 
 # Split string method
 import random
+from secrets import choice
+from tkinter import N
+from unicodedata import name
 
 
-names_string = input("Give me everybody's names, separated by a comma. ")
+# names_string = input("Give me everybody's names, separated by a comma. ")
+names_string = "todd, scott, tracy, tabby, logan, alyssa"
 names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
 
-num_names = random.names
-print(num_names)
+
+# print(names)
+# len_names = len(names) - 1
+if "joe" in names:
+    names.remove("joe")
+# if pick eq joe ---remove joe and pick again
+u_choice = random.randint(0, len(names) - 1)
+print(f"{names[u_choice]} is going to buy the meal today")
+
+
+
+
+# if u_choice == 6:
+#     print("I picked joe, lol")
+#     u_choice = random.randint(0, len(names) - 1)
+
+
 
 ### Working Code Below
 # numb_names = len(names)
